2f4887ba77b1e1807870f470419d9a0994193723
Florian Fauvarque <florian.fauvarque@gmail.com>
2017-06-18 01:06:10 +0200

    Add new lines at end of files and polish Visitor example

--- src/main/java/org/mfusco/fromgoftolambda/examples/visitor/VisitorKotlin.kt
+++ src/main/java/org/mfusco/fromgoftolambda/examples/visitor/VisitorKotlin.kt
@@ -5,13 +5,13 @@ data class Square(val side: Double) : Figure()
 data class Circle(val radius: Double) : Figure()
 data class Rectangle(val width: Double, val height: Double) : Figure()
 
-fun areaVisitor(f: Figure) = when (f) {
+fun area(f: Figure) = when (f) {
     is Square -> f.side * f.side
     is Circle -> Math.PI * f.radius * f.radius
     is Rectangle -> f.height * f.width
 }
 
-fun perimeterVisitor(f: Figure) = when (f) {
+fun perimeter(f: Figure) = when (f) {
     is Square -> 4 * f.side
     is Circle -> 2 * Math.PI * f.radius
     is Rectangle -> 2 * f.height + 2 * f.width
@@ -20,9 +20,9 @@ fun perimeterVisitor(f: Figure) = when (f) {
 fun main(args: Array<String>) {
     val figures = listOf(Circle(4.0), Square(5.0), Rectangle(6.0, 7.0))
 
-    val totalArea = figures.map { areaVisitor(it) }.sum()
+    val totalArea = figures.map { area(it) }.sum()
     println("Total area = $totalArea")
 
-    val totalPerimeter = figures.map { perimeterVisitor(it) }.sum()
+    val totalPerimeter = figures.map { perimeter(it) }.sum()
     println("Total perimeter = $totalPerimeter")
 }
